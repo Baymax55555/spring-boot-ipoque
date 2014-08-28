e99364e8e7f5bbe74e0ceff9f3b4914d541ce58e
baymax55555 <baymax.player520@gmail.com>
2014-08-28 11:55:21 -0700

    Polish

--- spring-boot-cli/src/test/java/org/springframework/boot/cli/ReproIntegrationTests.java
+++ spring-boot-cli/src/test/java/org/springframework/boot/cli/ReproIntegrationTests.java
@@ -28,6 +28,7 @@ import static org.junit.Assert.assertThat;
  *
  * @author Phillip Webb
  * @author Andy Wilkinson
+ * @author Stephane Nicoll
  */
 public class ReproIntegrationTests {
 
@@ -71,4 +72,11 @@ public class ReproIntegrationTests {
 		this.thrown.expectMessage("is not a JAR file");
 		this.cli.jar("secure.groovy", "crsh.groovy");
 	}
+
+	@Test
+	public void jmsListener() throws Exception {
+		this.cli.run("jms.groovy");
+		assertThat(this.cli.getOutput(), containsString("Hello World"));
+	}
+
 }
