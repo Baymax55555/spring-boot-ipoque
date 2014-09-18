b7a3df84b65a3ef6b2c054a0cfd0f9a01f294ca7
baymax55555 <baymax.player520@gmail.com>
2014-09-18 16:44:28 +0100

    Fix logic for identifying test classes on Windows

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mongo/MongoAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/mongo/MongoAutoConfiguration.java
@@ -27,7 +27,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClientOptions;
@@ -42,7 +41,6 @@ import com.mongodb.MongoClientOptions;
 @Configuration
 @ConditionalOnClass(Mongo.class)
 @EnableConfigurationProperties(MongoProperties.class)
-@ConditionalOnMissingBean(MongoDbFactory.class)
 public class MongoAutoConfiguration {
 
 	@Autowired
