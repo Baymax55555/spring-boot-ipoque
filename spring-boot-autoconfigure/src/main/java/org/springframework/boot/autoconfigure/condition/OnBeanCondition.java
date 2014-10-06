bbb4c142dccac420c892d09554e86494a62e49fa
baymax55555 <baymax.player520@gmail.com>
2014-10-06 12:03:51 -0700

    Polish

--- spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/condition/OnBeanCondition.java
+++ spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/condition/OnBeanCondition.java
@@ -37,7 +37,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.ConfigurationCondition;
+import org.springframework.core.Ordered;
 import org.springframework.core.ResolvableType;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.core.type.MethodMetadata;
 import org.springframework.util.Assert;
@@ -54,6 +56,7 @@ import org.springframework.util.StringUtils;
  * @author Dave Syer
  * @author Jakub Kubrynski
  */
+@Order(Ordered.LOWEST_PRECEDENCE)
 class OnBeanCondition extends SpringBootCondition implements ConfigurationCondition {
 
 	/**
@@ -290,6 +293,7 @@ class OnBeanCondition extends SpringBootCondition implements ConfigurationCondit
 					.getAllAnnotationAttributes(annotationType.getName(), true);
 			collect(attributes, "name", this.names);
 			collect(attributes, "value", this.types);
+			collect(attributes, "type", this.types);
 			collect(attributes, "annotation", this.annotations);
 			if (this.types.isEmpty() && this.names.isEmpty()) {
 				addDeducedBeanType(context, metadata, this.types);
