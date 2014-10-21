df1c5147c353df2447b8ab8ba398c0658481719d
baymax55555 <baymax.player520@gmail.com>
2014-10-21 16:07:15 +0100

    Update WebSocket sample with new SockJS CDN

--- spring-boot-actuator/src/main/java/org/springframework/boot/actuate/autoconfigure/ManagementSecurityAutoConfiguration.java
+++ spring-boot-actuator/src/main/java/org/springframework/boot/actuate/autoconfigure/ManagementSecurityAutoConfiguration.java
@@ -30,12 +30,10 @@ import org.springframework.boot.actuate.endpoint.mvc.MvcEndpoint;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionOutcome;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
 import org.springframework.boot.autoconfigure.security.AuthenticationManagerConfiguration;
 import org.springframework.boot.autoconfigure.security.FallbackWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
@@ -46,11 +44,8 @@ import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -170,35 +165,16 @@ public class ManagementSecurityAutoConfiguration {
 	}
 
 	@Configuration
+	@ConditionalOnExpression("${management.security.enabled:true} && !${security.basic.enabled:true}")
 	@ConditionalOnMissingBean(WebSecurityConfiguration.class)
 	@ConditionalOnWebApplication
-	@Conditional(WebSecurityEnablerCondition.class)
 	@EnableWebSecurity
 	protected static class WebSecurityEnabler extends AuthenticationManagerConfiguration {
 	}
 
-	/**
-	 * WebSecurityEnabler condition
-	 */
-	static class WebSecurityEnablerCondition extends SpringBootCondition {
-
-		@Override
-		public ConditionOutcome getMatchOutcome(ConditionContext context,
-				AnnotatedTypeMetadata metadata) {
-			String managementEnabled = context.getEnvironment().getProperty(
-					"management.security.enabled", "true");
-			String basicEnabled = context.getEnvironment().getProperty(
-					"security.basic.enabled", "true");
-			return new ConditionOutcome("true".equalsIgnoreCase(managementEnabled)
-					&& !"true".equalsIgnoreCase(basicEnabled),
-					"Management security enabled and basic disabled");
-		}
-
-	}
-
 	@Configuration
 	@ConditionalOnMissingBean({ ManagementWebSecurityConfigurerAdapter.class })
-	@ConditionalOnProperty(prefix = "management.security", name = "enabled", matchIfMissing = true)
+	@ConditionalOnExpression("${management.security.enabled:true}")
 	@ConditionalOnWebApplication
 	@Order(ManagementServerProperties.BASIC_AUTH_ORDER)
 	protected static class ManagementWebSecurityConfigurerAdapter extends
--- spring-boot/src/test/java/org/springframework/boot/test/SpringApplicationContextLoaderTests.java
+++ spring-boot/src/test/java/org/springframework/boot/test/SpringApplicationContextLoaderTests.java
@@ -16,16 +16,15 @@
 
 package org.springframework.boot.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.util.Map;
 
 import org.junit.Test;
 import org.springframework.test.context.MergedContextConfiguration;
-import org.springframework.test.context.TestContext;
-import org.springframework.test.context.TestContextManager;
-import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 /**
  * Tests for {@link SpringApplicationContextLoader}
@@ -37,57 +36,30 @@ public class SpringApplicationContextLoaderTests {
 	private final SpringApplicationContextLoader loader = new SpringApplicationContextLoader();
 
 	@Test
-	public void environmentPropertiesSimple() throws Exception {
+	public void environmentPropertiesSimple() {
 		Map<String, Object> config = getEnvironmentProperties(SimpleConfig.class);
 		assertKey(config, "key", "myValue");
 		assertKey(config, "anotherKey", "anotherValue");
 	}
 
 	@Test
-	public void environmentPropertiesDefaults() throws Exception {
-		Map<String, Object> config = getEnvironmentProperties(SimpleConfig.class);
-		assertMissingKey(config, "server.port");
-		assertKey(config, "spring.jmx.enabled", "false");
-	}
-
-	@Test
-	public void environmentPropertiesOverrideDefaults() throws Exception {
-		Map<String, Object> config = getEnvironmentProperties(OverrideConfig.class);
-		assertKey(config, "server.port", "2345");
-	}
-
-	@Test(expected=IllegalStateException.class)
-	public void environmentPropertiesIllegal() throws Exception {
-		getEnvironmentProperties(IllegalConfig.class);
-	}
-
-	@Test
-	public void environmentPropertiesAppend() throws Exception {
-		Map<String, Object> config = getEnvironmentProperties(AppendConfig.class);
-		assertKey(config, "key", "myValue");
-		assertKey(config, "otherKey", "otherValue");
-	}
-
-	@Test
-	public void environmentPropertiesSeparatorInValue() throws Exception {
+	public void environmentPropertiesSeparatorInValue() {
 		Map<String, Object> config = getEnvironmentProperties(SameSeparatorInValue.class);
 		assertKey(config, "key", "my=Value");
 		assertKey(config, "anotherKey", "another:Value");
 	}
 
 	@Test
-	public void environmentPropertiesAnotherSeparatorInValue() throws Exception {
+	public void environmentPropertiesAnotherSeparatorInValue() {
 		Map<String, Object> config = getEnvironmentProperties(AnotherSeparatorInValue.class);
 		assertKey(config, "key", "my:Value");
 		assertKey(config, "anotherKey", "another=Value");
 	}
 
-	private Map<String, Object> getEnvironmentProperties(Class<?> testClass) throws Exception {
-		TestContext context = new ExposedTestContextManager(testClass).getExposedTestContext();
-		new IntegrationTestPropertiesListener().prepareTestInstance(context);
-		MergedContextConfiguration config = (MergedContextConfiguration) ReflectionTestUtils.getField(
-				context, "mergedContextConfiguration");
-		return this.loader.extractEnvironmentProperties(config.getPropertySourceProperties());
+	private Map<String, Object> getEnvironmentProperties(Class<?> testClass) {
+		MergedContextConfiguration configuration = mock(MergedContextConfiguration.class);
+		doReturn(testClass).when(configuration).getTestClass();
+		return this.loader.getEnvironmentProperties(configuration);
 	}
 
 	private void assertKey(Map<String, Object> actual, String key, Object value) {
@@ -95,24 +67,8 @@ public class SpringApplicationContextLoaderTests {
 		assertEquals(value, actual.get(key));
 	}
 
-	private void assertMissingKey(Map<String, Object> actual, String key) {
-		assertTrue("Key '" + key + "' found", !actual.containsKey(key));
-	}
-
 	@IntegrationTest({ "key=myValue", "anotherKey:anotherValue" })
 	static class SimpleConfig {
-	}
-
-	@IntegrationTest({ "server.port=2345" })
-	static class OverrideConfig {
-	}
-
-	@IntegrationTest(value = { "key=aValue", "anotherKey:anotherValue" }, properties = { "key=myValue", "otherKey=otherValue" })
-	static class IllegalConfig {
-	}
-
-	@IntegrationTest(properties = { "key=myValue", "otherKey=otherValue" })
-	static class AppendConfig {
 	}
 
 	@IntegrationTest({ "key=my=Value", "anotherKey:another:Value" })
@@ -121,19 +77,6 @@ public class SpringApplicationContextLoaderTests {
 
 	@IntegrationTest({ "key=my:Value", "anotherKey:another=Value" })
 	static class AnotherSeparatorInValue {
-	}
-	
-	private static class ExposedTestContextManager extends TestContextManager {
-
-		public ExposedTestContextManager(Class<?> testClass) {
-			super(testClass);
-		}
-		
-		public final TestContext getExposedTestContext() {
-			return super.getTestContext();
-		}
-		
-		
 	}
 
 }
