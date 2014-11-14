afa1d7dcab29ec3994eea27ad2805b4d544f77c0
baymax55555 <baymax.player520@gmail.com>
2014-11-13 18:44:10 -0800

    Sync up versions used in Maven integration tests

--- spring-boot-samples/spring-boot-sample-flyway/src/main/java/sample/flyway/SampleFlywayApplication.java
+++ spring-boot-samples/spring-boot-sample-flyway/src/main/java/sample/flyway/SampleFlywayApplication.java
@@ -23,11 +23,15 @@ import javax.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class SampleFlywayApplication implements CommandLineRunner {
 
 	@Autowired
--- spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/ExcludeFilter.java
+++ spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/ExcludeFilter.java
@@ -16,32 +16,73 @@
 
 package org.springframework.boot.maven;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.shared.artifact.filter.collection.AbstractArtifactsFilter;
+import org.apache.maven.shared.artifact.filter.collection.ArtifactFilterException;
 
 /**
- * An {@ArtifactsFilter} that filters out any artifact matching an
- * {@link Exclude}.
+ * An {@link org.apache.maven.shared.artifact.filter.collection.ArtifactsFilter
+ * ArtifactsFilter} that filters out any artifact matching a configurable list of
+ * {@link Exclude} instances.
  *
  * @author Stephane Nicoll
- * @author David Turanski
  * @since 1.1
  */
-public class ExcludeFilter extends DependencyFilter {
+public class ExcludeFilter extends AbstractArtifactsFilter {
 
+	private final List<Exclude> excludes;
+
+	/**
+	 * Create a new instance with the list of {@link Exclude} instance(s) to use.
+	 */
 	public ExcludeFilter(List<Exclude> excludes) {
-		super(excludes);
+		this.excludes = excludes;
 	}
 
 	@Override
-	protected boolean filter(Artifact artifact) {
-		for (FilterableDependency dependency : getFilters()) {
-			if (equals(artifact, dependency)) {
+	@SuppressWarnings("rawtypes")
+	public Set filter(Set artifacts) throws ArtifactFilterException {
+		Set<Artifact> result = new HashSet<Artifact>();
+		for (Object a : artifacts) {
+			Artifact artifact = (Artifact) a;
+			if (!matchExclude(artifact)) {
+				result.add(artifact);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Check if the specified {@link Artifact} matches one of the known excludes. Returns
+	 * {@code true} if it should be excluded
+	 */
+	private boolean matchExclude(Artifact artifact) {
+		for (Exclude exclude : this.excludes) {
+			if (match(artifact, exclude)) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	/**
+	 * Check if the specified {@link Artifact} matches the specified {@link Exclude}.
+	 * Returns {@code true} if it should be excluded
+	 */
+	private boolean match(Artifact artifact, Exclude exclude) {
+		if (!exclude.getGroupId().equals(artifact.getGroupId())) {
+			return false;
+		}
+		if (!exclude.getArtifactId().equals(artifact.getArtifactId())) {
+			return false;
+		}
+		return (exclude.getClassifier() == null || artifact.getClassifier() != null
+				&& exclude.getClassifier().equals(artifact.getClassifier()));
+
 	}
 
 }
